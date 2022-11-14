bf4ea90651f2a17125513fb57d3ef9909a76d554
bavila19 <brendaavila015@gmail.com>
2022-11-13 17:52:29 -0700

    Name element

--- main.js
+++ main.js
@@ -7,3 +7,17 @@
     overlay.style.display = 'none';
     popup.style.display = 'none';
 };
+//next goal is to add a new element for 
+const form = document.querySelector("#form");
+form.addEventListener("submit", (event)=> {
+    event.preventDefault()
+    //console.log(event.target)
+    const commentInput = document.querySelector('#text')
+    const commentText=  commentInput.value
+    console.log(commentInput.value)
+    const newComment= document.createElement('li')
+    newComment.innerHTML = commentText
+    console.log(newComment)
+    document.querySelector('ul').appendChild(newComment)
+    commentInput.value=""
+});
